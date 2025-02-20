69f362336d62a28235608e780b35466059a30d46
karlijn76x <karlijnvandenherik@gmail.com>
2025-02-20 13:46:40 +0100

    Initial Commit

---
+++ TechCase20/TechCase20App.swift
@@ -0,0 +1,17 @@
+//
+//  TechCase20App.swift
+//  TechCase20
+//
+//  Created by Karlijn van den Herik on 20/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct TechCase20App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
